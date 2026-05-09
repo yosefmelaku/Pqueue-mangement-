838aed2812d7c93fe87e67eb9ef62b1a0b2b1af9
yosefmelaku <yosefmelaku835@gmail.com>
2026-05-09 11:43:58 +0300

    add some chage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // MIME types for different file extensions
 const mimeTypes = {
